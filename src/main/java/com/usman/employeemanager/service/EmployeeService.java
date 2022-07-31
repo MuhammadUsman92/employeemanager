205539a8e82d7abf5ff64c56be0df60f7796132d
MuhammadUsman92 <106598317+MuhammadUsman92@users.noreply.github.com>
2022-07-31 19:42:51 +0500

    Final Commit

---
+++ src/main/java/com/usman/employeemanager/service/EmployeeService.java
@@ -0,0 +1,38 @@
+package com.usman.employeemanager.service;
+
+import com.usman.employeemanager.exception.UserNotFoundException;
+import com.usman.employeemanager.model.Employee;
+import com.usman.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(()->new UserNotFoundException("User by id "+id+" was not found"));
+    }
+}
